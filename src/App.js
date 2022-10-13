7ed67396153e18791943feb07236958d8c4d936c
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-13 09:57:01 +0100

    Opened google login popup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase/firebase.init'
 
 const auth = getAuth(app);
@@ -8,7 +8,14 @@ function App() {
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
-    console.log('hi')
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('error: ', error);
+      })
   }
   return (
     <div className="App">
